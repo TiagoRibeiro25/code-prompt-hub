e478cc0c612c7e0dbfd7f3efcceb44afae36928c
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2025-03-18 17:36:59 +0000

    Added missing error handler

--- src/app/api/exercise/route.ts
+++ src/app/api/exercise/route.ts
@@ -18,58 +18,67 @@ type PostRequestBody = {
 };
 
 export async function POST(req: NextRequest) {
-  const requestBody: PostRequestBody = await req.json();
-  const requestBodyValidation = validateSchema(
-    POST_REQUEST_SCHEMA,
-    requestBody
-  );
-
-  if (!requestBodyValidation.isValid) {
-    return NextResponse.json(
-      { error: requestBodyValidation.error },
-      { status: 400 }
+  try {
+    const requestBody: PostRequestBody = await req.json();
+    const requestBodyValidation = validateSchema(
+      POST_REQUEST_SCHEMA,
+      requestBody,
     );
-  }
 
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+    if (!requestBodyValidation.isValid) {
+      return NextResponse.json(
+        { error: requestBodyValidation.error },
+        { status: 400 },
+      );
+    }
 
-  const userId = await getUserIdOnServer(session);
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
-  // Check if the user has more than 5 uncompleted exercises
-  const userUnfinishedExercises = await prisma.exercise.findMany({
-    where: {
-      user_id: userId,
-      score: { equals: null },
-    },
-    select: { id: true },
-  });
+    const userId = await getUserIdOnServer(session);
 
-  if (userUnfinishedExercises.length >= 5) {
-    return NextResponse.json(
-      {
-        error:
-          "You have too many unfinished exercises. Complete or delete them before creating a new one.",
+    // Check if the user has more than 5 uncompleted exercises
+    const userUnfinishedExercises = await prisma.exercise.findMany({
+      where: {
+        user_id: userId,
+        score: { equals: null },
       },
-      { status: 400 }
+      select: { id: true },
+    });
+
+    if (userUnfinishedExercises.length >= 5) {
+      return NextResponse.json(
+        {
+          error:
+            "You have too many unfinished exercises. Complete or delete them before creating a new one.",
+        },
+        { status: 400 },
+      );
+    }
+
+    const code = await generateExercise(requestBody.difficulty);
+
+    // Create exercise
+    const exercise = await prisma.exercise.create({
+      data: {
+        user_id: userId,
+        code,
+        difficulty: requestBody.difficulty,
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Exercise Created", exercise },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.log("500 error:", error);
+
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
     );
   }
-
-  const code = await generateExercise(requestBody.difficulty);
-
-  // Create exercise
-  const exercise = await prisma.exercise.create({
-    data: {
-      user_id: userId,
-      code,
-      difficulty: requestBody.difficulty,
-    },
-  });
-
-  return NextResponse.json(
-    { message: "Exercise Created", exercise },
-    { status: 201 }
-  );
 }
